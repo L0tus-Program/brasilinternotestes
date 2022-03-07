dc03d12527fb2af89e771be432b4231d00063fed
Felipe Gomes <62225877+L0tus-Program@users.noreply.github.com>
2022-03-07 15:16:58 -0300

    Mudança de layout
    Adicionado o index2 onde estarei desenvolvendo um novo layout para a versão 2.0 do portal interno + mudanças no CSS deixando o Bootstrap mais puro nas páginas.

--- scripts/avisos.js
+++ scripts/avisos.js
@@ -1,9 +1,10 @@
- texto = "Olá , seja muito bem - vindo ao portal interno da Brasil dos Parafusos ! \nAqui você irá encontrar todos os dados , arquivos e informações que irão te ajudar no dia a dia !"
+ //texto = "Olá , seja muito bem - vindo ao portal interno da Brasil dos Parafusos ! \nAqui você irá encontrar todos os dados , arquivos e informações que irão te ajudar no dia a dia !"
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    alert(texto);
+    //alert(texto);
+    $('#bemvindo').modal('show');
 }, false);
 
 
